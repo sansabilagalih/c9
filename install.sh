bbaf1c04b1536f7a6339f33ce2cb1f16a96b1dfa
sansabilagalih <sansabillagalih@gmail.com>
2024-08-21 21:21:48 +0700

    Update install.sh
    Tambah c9-master

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo apt upgrade -y
 sudo apt -y install curl git build-essential unzip nodejs make gcc python2-minimal libapache2-mod-php7.2 php7.2 php7.2-common
 
 # Ekstrak file c9.zip ke direktori c9sdk
-unzip c9.zip -d c9sdk
+unzip c9.zip -d c9sdk/c9-master
 
 # Masuk ke direktori c9sdk dan install SDK
 cd c9sdk
