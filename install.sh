ea0c6b95778d670c465c5ed2f4d729e3d5854424
sansabilagalih <sansabillagalih@gmail.com>
2024-08-21 21:12:25 +0700

    Add C9 & Add Shell Script
    Add C9 file & Add shell script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Tambahkan repository dan update sistem
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt-add-repository universe -y
+sudo apt update -y
+sudo apt upgrade -y
+
+# Install dependencies
+sudo apt -y install curl git build-essential unzip nodejs make gcc python2-minimal libapache2-mod-php7.2 php7.2 php7.2-common
+
+# Ekstrak file c9.zip ke direktori c9sdk
+unzip c9.zip -d c9sdk
+
+# Masuk ke direktori c9sdk dan install SDK
+cd c9sdk
+scripts/install-sdk.sh
+
+# Jalankan server Cloud9
+nodejs server.js -p 8080 -l 0.0.0.0 -a 123:123 -w /home
